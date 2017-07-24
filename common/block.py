7c36ced51e4deceaccac5f4c376115a22935b4a1
Ilan Dubois <ilan.dubois@epita.fr>
2017-07-24 14:47:02 +0200

    Add random initial pad in block and getPack method

--- common/block.py
+++ common/block.py
@@ -1,6 +1,12 @@
+import random
+import struct
+
 class Block:
 
     def __init__(self, prev, root):
         self.prev = prev
         self.root = root
-        self.pad = 0
+        self.pad = random.randrange(2 ** 64)
+    
+    def getPack(self):
+        return struct.pack('ssQ', self.prev, self.root, self.pad)
