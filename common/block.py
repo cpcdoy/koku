378856c96711885d09e86c200eaab45a120b644f
Chady Dimachkie <cpcdoy@gmail.com>
2017-07-24 19:53:15 +0200

    Fix?: some types errors within the Block structure

--- common/block.py
+++ common/block.py
@@ -8,12 +8,16 @@ class Block:
     def __init__(self, prev, root):
         self.prev = prev
         self.root = root
-        self.time = time.gmtime()
+        self.time = int(round(time.time()))
+        #self.time = time.gmtime()
         self.bits = 20
         self.pad = random.randrange(2 ** 32)
 
     def getPack(self):
-        return struct.pack('ssIII', self.prev, self.root, self.time, self.bits, self.pad)
+        prev_encode = self.prev.encode()
+        root_encode = self.root.encode()
+
+        return struct.pack('ssIII', prev_encode, root_encode, self.time, self.bits, self.pad)
 
     def checkChain(chain):
         prev = None
