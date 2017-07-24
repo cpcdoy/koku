21db19ba0902a6e770b9087c40f16c9d881ff14c
Ilan Dubois <ilan.dubois@epita.fr>
2017-07-24 15:28:17 +0200

    Nonce is now a 32 bits integer

--- common/block.py
+++ common/block.py
@@ -6,7 +6,7 @@ class Block:
     def __init__(self, prev, root):
         self.prev = prev
         self.root = root
-        self.pad = random.randrange(2 ** 64)
-    
+        self.pad = random.randrange(2 ** 32)
+
     def getPack(self):
-        return struct.pack('ssQ', self.prev, self.root, self.pad)
+        return struct.pack('ssI', self.prev, self.root, self.pad)
