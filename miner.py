7d12853688a2d989d154803aca145b92d32ab0a2
Ilan Dubois <ilan.dubois@epita.fr>
2017-07-25 16:12:20 +0200

    Add simple daemon to the miner script

--- miner.py
+++ miner.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+import daemon
 from common.address import *
 from common.block import Block
 from optparse import OptionParser
 from common.transaction import Transaction
+
+def miner():
+    sk = ecdsa.SigningKey.from_pem(f.read())
+    addr = getAddr(sk.get_verifying_key())
 
 if __name__ == "__main__":
     parser = OptionParser(description="This script is useed to mine the Koku crypto-currency.")
@@ -12,3 +17,6 @@ if __name__ == "__main__":
 
     if args.key:
         print("Your address is:", genKey())
+    else:
+        with daemon.DaemonContext():
+            miner()
